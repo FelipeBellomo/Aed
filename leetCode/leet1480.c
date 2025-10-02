f20ebd4bcfe1867cb6125c4d2f3d38cc192c3391
Felipe Bellomo <felipepereira1335@gmail.com>
2025-10-02 15:16:22 +0000

    update

--- leetCode/leet1480.c
+++ leetCode/leet1480.c
@@ -2,7 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 
+int* runningSum(int* nums, int numsSize, int* returnSize);
+
 int main() {
+    int nums[4] = {1,2,3,4};
+    int size = 4;
+    int retunrSize;
     
-    return 0;
+
+    int* ret = runningSum(nums,size,&retunrSize);
+
+    for (int i = 0; i < retunrSize; i++){
+        printf("%d",ret[i]);
+    }
+    
+
+}
+
+int* runningSum(int* nums, int numsSize, int* returnSize) {
+    *returnSize = numsSize;
+    int* result = (int*)malloc(sizeof(int)* (*returnSize));
+ 
+
+    result[0] = nums[0];
+
+    for (int i = 1; i < numsSize; i++){
+        result[i] = result[i - 1] + nums[i]; 
+        printf("%d\n",result[i]);
+    }
+
+    return result;
+    
 }
